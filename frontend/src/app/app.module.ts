469271c8f16d7bbf00d71873c86b18cd521115f7
wazatoki <wazatoki@gmail.com>
2019-09-29 09:45:41 +0900

    layoutのテンプレートを作成

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,12 +7,18 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { WelcomComponent } from './welcom/welcom.component';
+import { LayoutHeaderSidebarContentsComponent } from './layout/layout-header-sidebar-contents/layout-header-sidebar-contents.component';
+import { LayoutHeaderContentsComponent } from './layout/layout-header-contents/layout-header-contents.component';
+import { LayoutContentsComponent } from './layout/layout-contents/layout-contents.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     WelcomComponent,
+    LayoutHeaderSidebarContentsComponent,
+    LayoutHeaderContentsComponent,
+    LayoutContentsComponent,
   ],
   imports: [
     BrowserModule,
