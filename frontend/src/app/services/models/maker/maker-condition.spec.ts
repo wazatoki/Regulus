b682669a899b6394accfc5f324aaf6fd26e72d97
wazatoki <wazatoki@gmail.com>
2019-10-26 11:45:48 +0900

    maker-condition toMap method作成

--- frontend/src/app/services/models/maker/maker-condition.spec.ts
+++ frontend/src/app/services/models/maker/maker-condition.spec.ts
@@ -7,13 +7,45 @@ describe('MakerCondition', () => {
   beforeEach(() => {
 
     TestBed.configureTestingModule({
-      providers:[
+      providers: [
         Maker
       ]
     });
   });
+
   it('should create an instance', () => {
-    const makerCondition = TestBed.get(MakerCondition)
+    const makerCondition = TestBed.get(MakerCondition);
     expect(makerCondition).toBeTruthy();
   });
+
+  it('test property id', () => {
+    const makerCondition: MakerCondition = TestBed.get(MakerCondition);
+    makerCondition.id = 'testid';
+    expect(makerCondition.id).toEqual('testid');
+  });
+
+  it('test property name', () => {
+    const makerCondition: MakerCondition = TestBed.get(MakerCondition);
+    makerCondition.name = 'testname';
+    expect(makerCondition.name).toEqual('testname');
+  });
+
+  it('test toMap with full member', () => {
+    const makerCondition: MakerCondition = TestBed.get(MakerCondition);
+    makerCondition.id = 'testid';
+    makerCondition.name = 'testname';
+    const result: Map<string, string> = new Map();
+    result.set('id', 'testid');
+    result.set('name', 'testname');
+    expect(makerCondition.toMap()).toEqual(result);
+  });
+
+  it('test toMap with undefind member', () => {
+    const makerCondition: MakerCondition = TestBed.get(MakerCondition);
+    makerCondition.id = 'testid';
+    const result: Map<string, string> = new Map();
+    result.set('id', 'testid');
+    expect(makerCondition.toMap()).toEqual(result);
+  });
+
 });
--- frontend/src/app/services/models/maker/maker-condition.ts
+++ frontend/src/app/services/models/maker/maker-condition.ts
@@ -8,5 +8,33 @@ export class MakerCondition {
 
     constructor(private maker: Maker) { }
 
-    
+    get id(): string {
+        return this.maker.id;
+    }
+
+    set id(id: string) {
+        this.maker.id = id;
+    }
+
+    get name(): string {
+        return this.maker.name;
+    }
+
+    set name(name: string) {
+        this.maker.name = name;
+    }
+
+    toMap(): Map<string, string> {
+        const result: Map<string, string> = new Map();
+
+        if (this.id) {
+            result.set('id', this.id);
+        }
+
+        if (this.name) {
+            result.set('name', this.name);
+        }
+
+        return result;
+    }
 }
