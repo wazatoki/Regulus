9617f31846202db5716b17581e54771377a4493a
wazatoki <wazatoki@gmail.com>
2019-11-03 10:00:21 +0900

    maker-input-form dialog処理作成中

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button'; 
+import { MatDialog } from '@angular/material/dialog';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -22,6 +23,7 @@ import { LayoutModule } from './layout/layout.module';
     BrowserAnimationsModule,
     MatButtonModule,
     MatMenuModule,
+    MatDialog,
     LayoutModule,
   ],
   providers: [],
--- frontend/src/app/maker/maker-input-form/maker-input-form.component.ts
+++ frontend/src/app/maker/maker-input-form/maker-input-form.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Inject } from '@angular/core';
+import { Maker } from '../../services/models/maker/maker';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-maker-input-form',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MakerInputFormComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  maker: Maker;
+
+  constructor(
+    private fb: FormBuilder,
+    private dialogRef: MatDialogRef<MakerInputFormComponent>,
+    @Inject(MAT_DIALOG_DATA) data
+  ) {
+    this.maker = data.maker;
+   }
 
   ngOnInit() {
+    this.form = this.fb.group(this.maker)
+  }
+
+  onCancelClick(){
+    this.dialogRef.close(this.maker)
+  }
+
+  onSaveClick(){
+    this.dialogRef.close();
   }
 
 }
