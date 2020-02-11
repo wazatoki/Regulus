490021272364d996d92b3b4c2670487767cabe4a
wazatoki <wazatoki@gmail.com>
2020-02-11 20:22:51 +0900

    雛形作成

---
+++ frontend/src/app/maker/maker-complex-search/maker-complex-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-maker-complex-search',
+  templateUrl: './maker-complex-search.component.html',
+  styleUrls: ['./maker-complex-search.component.css']
+})
+export class MakerComplexSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
