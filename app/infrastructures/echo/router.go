177d391d446d829f675024acb0f92831ebab430e
wazatoki <wazatoki@gmail.com>
2020-07-12 20:05:56 +0900

    gatewayディレクトリ廃止

--- app/infrastructures/echo/router.go
+++ app/infrastructures/echo/router.go
@@ -1,12 +1,14 @@
 package echo
 
 import (
+	"regulus/app/infrastructures/echo/handlers"
+
 	"github.com/labstack/echo"
 )
 
 func defineRouting(e *echo.Echo) {
-	//apiPath := "/api"
-	e.GET("/", root)
-	e.GET("/index", index)
-	//e.GET(apiPath+"/maker/ComplexSearchItems", handle.MakerComplexSearchItems)
+	//apiPath := "/api/"
+	e.GET("/", handlers.Root)
+	e.GET("/index", handlers.Index)
+	//e.GET(apiPath+"maker/ComplexSearchItems", handle.MakerComplexSearchItems)
 }
