ef65c01e4a32242316a75638a4ffb88ad85e0212
wazatoki <wazatoki@gmail.com>
2021-01-09 15:07:18 +0900

    direnvの設定ファイル追加　go tools のインストールを変更

--- tools/install_go_tools.sh
+++ tools/install_go_tools.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
 # mage
-go get -v -u -d github.com/magefile/mage
-cd $GOPATH/src/github.com/magefile/mage
+GO111MODULE=off go get -u -d github.com/magefile/mage
+cd $HOME/go/1.3.1/src/github.com/magefile/mage
 go run bootstrap.go
 
 # sqlboiler
-go get -v -u -t github.com/volatiletech/sqlboiler
-go get -v -u github.com/volatiletech/sqlboiler/drivers/sqlboiler-psql
+# Install sqlboiler v4
+GO111MODULE=off go get -u -t github.com/volatiletech/sqlboiler
+# Install an sqlboiler driver - these are seperate binaries, here we are
+# choosing postgresql
+GO111MODULE=off go get github.com/volatiletech/sqlboiler/drivers/sqlboiler-psql
 
 # sql-migrate
-go get -v -u github.com/rubenv/sql-migrate/...
+GO111MODULE=off go get -v -u github.com/rubenv/sql-migrate/...
